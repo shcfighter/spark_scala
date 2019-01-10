0d944ed1f67493e06b66ef986ae4f744af5f0d1d
shc_fighter <shc_fighter@163.com>
2019-01-10 15:18:28 +0800

    avg

--- src/main/scala/com/ecit/spark_scala/Avg.scala
+++ src/main/scala/com/ecit/spark_scala/Avg.scala
@@ -7,7 +7,9 @@ object Avg {
     var conf = new SparkConf();
     var sc = new SparkContext(conf);
     var rdd = sc.textFile("file:///root/book.txt");
-    var rdd2 = rdd.map(line => line.split(","))
+    var rdd2 = rdd.map(line => line.replace(",", " "))
     rdd2.foreach(line => println(line))
+    println("================================================================================")
+    rdd.flatMap(line => line.replace(",", " ")).foreach(line => println(line))
   }
 }
