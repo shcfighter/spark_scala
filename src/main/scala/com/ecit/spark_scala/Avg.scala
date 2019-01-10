c6e45baec1c0f1799cba0ef82c608e691304ff60
shc_fighter <shc_fighter@163.com>
2019-01-10 16:50:26 +0800

    avg

--- src/main/scala/com/ecit/spark_scala/Avg.scala
+++ src/main/scala/com/ecit/spark_scala/Avg.scala
@@ -10,8 +10,16 @@ object Avg {
     var rdd2 = rdd.map(line => line.split(","))
     rdd2.foreach(line => println(line))
     println("================================================================================")
-    //rdd2.map(line => (line(0), line(1))).mapValues(line => (line, 1)).reduceByKey((x, y) => (x + y) / (x + y)).collect()
+    var rdd3 = rdd2.map(line => (line(0), line(1)));
+    rdd3.foreach(line => println(line))
+    println("================================================================================")
+    var rdd4 = rdd3.mapValues(line => (line, 1))
+    rdd4.foreach(line => println(line))
+    println("================================================================================")
+    var rdd5 = rdd4.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
+    rdd5.foreach(line => println(line))
+    println("================================================================================")
+    rdd5.mapValues(x => (x._1, x._2, x._1.toDouble / x._2.toDouble)).collect().foreach(line => println(line))
 
-    sc.parallelize(List(("a",10),("b",4),("a",10),("b",20))).mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues(x => (x._1, x._2, x._1.toDouble / x._2.toDouble)).collect().foreach(line => println(line))
   }
 }
