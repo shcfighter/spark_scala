63005b06ea9537d69d636cf9ee0969dc274c9e3f
shc_fighter <shc_fighter@163.com>
2019-01-10 16:37:27 +0800

    avg

--- src/main/scala/com/ecit/spark_scala/Avg.scala
+++ src/main/scala/com/ecit/spark_scala/Avg.scala
@@ -10,6 +10,8 @@ object Avg {
     var rdd2 = rdd.map(line => line.split(","))
     rdd2.foreach(line => println(line))
     println("================================================================================")
-    rdd2.map(line => (line(0), line(1))).map(line => (line, 1)).reduceByKey((x, y) => (x + y) / (x + y)).collect()
+    //rdd2.map(line => (line(0), line(1))).mapValues(line => (line, 1)).reduceByKey((x, y) => (x + y) / (x + y)).collect()
+
+    sc.parallelize(List(("a",10),("b",4),("a",10),("b",20))).mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).mapValues(x => (x._1, x._2, x._1.toDouble / x._2.toDouble)).collect().foreach(line => println(line))
   }
 }
