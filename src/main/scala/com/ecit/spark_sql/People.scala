3ab40c0a57b791a1141f0073f8db33c41b9454aa
shc_fighter <shc_fighter@163.com>
2019-01-11 10:35:03 +0800

    sql

--- src/main/scala/com/ecit/spark_sql/People.scala
+++
@@ -1,5 +0,0 @@
-package com.ecit.spark_sql
-
-class People {
-
-}
--- src/main/scala/com/ecit/spark_sql/TestSql.scala
+++ src/main/scala/com/ecit/spark_sql/TestSql.scala
@@ -1,5 +1,6 @@
 package com.ecit.spark_sql
 
+import com.ecit.spark_sql.SparkSQLExample.Person
 import org.apache.spark.sql.SparkSession
 
 object TestSql {
@@ -23,14 +24,22 @@ object TestSql {
   }
 
   def text(spark: SparkSession): Unit ={
-    case class Person(name: String, age: Long)
+    //case class Person(name: String, age: Long)
     import spark.implicits._
-    var df = spark.sparkContext
+    /*var df = spark.sparkContext
       .textFile("file:///root/people.txt")
       .map(_.split(","))
       .map(arr => Person(arr(1), arr(2).toLong)).toDF()
 
-    df.createTempView("people2")
+    df.createTempView("people2")*/
+
+    val peopleDF = spark.sparkContext
+      .textFile("file:///root/people.txt")
+      .map(_.split(","))
+      .map(attributes => Person(attributes(0), attributes(1).trim.toInt))
+      .toDF()
+    // Register the DataFrame as a temporary view
+    peopleDF.createOrReplaceTempView("people")
     spark.sql("select * from people2 where age > 0").show()
   }
 
