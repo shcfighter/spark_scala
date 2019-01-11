983ebcc6b7f2e05d89087959ecc3d9954f173f1e
shc_fighter <shc_fighter@163.com>
2019-01-11 15:56:41 +0800

    kafka

--- src/main/scala/com/ecit/spark_steaming/WordCount.scala
+++ src/main/scala/com/ecit/spark_steaming/WordCount.scala
@@ -12,13 +12,13 @@ import kafka.serializer.StringDecoder
 object WordCount {
   def main(args: Array[String]): Unit = {
     if (args.length < 2) {
-      println("WordCount zkserver1:9092, zkserver2:9092,zkserver3:9092 consumeMsgDataTimeInterval(secs)")
+      println("WordCount zkserver1:9092, zkserver2:9092,zkserver3:9092 consumeMsgDataTimeInterval(secs) topics")
       System.exit(1)
     }
     val sparkConf = new SparkConf().setAppName("WordCount").setMaster("local[2]") //至少2个线程，一个DRecive接受监听端口数据，一个计算
     val sc = new StreamingContext(sparkConf, Durations.seconds(args(1).toInt));
     val kafkaParams = Map[String, String]("metadata.broker.list" -> args(0)) // 然后创建一个set,里面放入你要读取的Topic,这个就是我们所说的,它给你做的很好,可以并行读取多个topic
-    var topics = Set[String]("linlin");
+    var topics = Set[String](args(2));
     //kafka返回的数据时key/value形式，后面只要对value进行分割就ok了
     val linerdd = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](sc, kafkaParams, topics)
     val wordrdd = linerdd.flatMap { _._2.split(" ") }
