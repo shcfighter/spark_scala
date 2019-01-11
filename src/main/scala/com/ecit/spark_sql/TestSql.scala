3b88488126d848d0df3251aa35f6f44a7dcb26e7
shc_fighter <shc_fighter@163.com>
2019-01-11 08:58:12 +0800

    sql

---
+++ src/main/scala/com/ecit/spark_sql/TestSql.scala
@@ -0,0 +1,14 @@
+package com.ecit.spark_sql
+
+import org.apache.spark.sql.SparkSession
+
+object TestSql {
+
+  def main(args: Array[String]): Unit = {
+    var sparkSession = SparkSession.builder().appName("app_spark_sql").getOrCreate();
+    var df = sparkSession.read.json("file:///root/people.json");
+    df.createTempView("people");
+    df.select("name", "age").show();
+  }
+
+}
